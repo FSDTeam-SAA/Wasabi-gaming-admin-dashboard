8097668613e75810512a9aa9d940d58a0443b4f7
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-01-14 09:13:38 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Login from '@/components/auth/Login'
-import Dashboard from '@/components/pages/Dashboard/Dashboard'
-import { redirect } from 'next/navigation'
-
+import Redirect from "@/components/redirect";
 export default function Home() {
-  redirect('/dashboard')
+  return (
+    <div>
+      <Redirect />
+    </div>
+  );
 }
